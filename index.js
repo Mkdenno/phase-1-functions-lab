6d63a6bfa5132e31cfa1168189e6fbd3202dbd7a
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-07 22:01:58 +0300

    Done

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
+function distanceFromHqInBlocks(someValue){
+   if(someValue<42){
+       return 42-someValue;
+   }
+   else{
+       return someValue-42;
+   }
+}
+function distanceFromHqInFeet(someValue){
+   return distanceFromHqInBlocks(someValue)*264;
 
-function distanceFromHqInBlocks (someNumber) {
-    if (someNumber > 42) {
-      return someNumber - 42;
-    } else {
-      return 42 - someNumber;
+}
+function distanceTravelledInFeet(start,destination){
+    if(start<destination){
+        return (destination-start)*264;
     }
-  }
-  
-  function distanceFromHqInFeet (someNumber) {
-    return distanceFromHqInBlocks(someNumber) * 264;
-  }
-  
-  function distanceTravelledInFeet (start, destination) {
-    if (start < destination) {
-      return (destination - start) * 264;
-    } else {
-      return (start - destination) * 264;
+    else{
+        return (start-destination)*264
     }
-  }
-  
-  function calculatesFarePrice (start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-  
-    if (distance <= 400) {
-      return 0;
-    } else if (distance > 400 && distance <= 2000) {
-      return 0.02 * (distance - 400);
-    } else if (distance > 2000 && distance < 2500) {
-      return 25;
-    } else {
-      return 'cannot travel that far';
+
+}
+function  calculatesFarePrice(start, destination){
+    const distance=distanceTravelledInFeet(start,destination);
+    if(distance<=400){
+        return 0;
     }
-  }
+    else if(distance>400 && distance <=2000){
+        return 0.02* (distance-400);
+    }
+    else if(distance>2000 && distance<2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
